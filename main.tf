c0856b96252089da8a48a46ed516b6307af5d9c7
liuyinvincent <liu.vincent@devoteam.com>
2024-08-06 19:02:58 +0200

    dynamic ip address

--- main.tf
+++ main.tf
@@ -4,9 +4,14 @@ provider "google" {
 }
 
 locals {
-  subnet_01_name = "${var.vpc_name}-subnet-01"
-  subnet_02_name = "${var.vpc_name}-subnet-02"
-  subnet_03_name = "${var.vpc_name}-subnet-03"
+  subnets = [for i in range(var.subnet_count) : {
+    subnet_name           = "${var.vpc_name}-subnet-${format("%02d", i + 1)}"
+    subnet_ip             = cidrsubnet(var.base_cidr_block, var.subnet_prefix_length - length(split("/", var.base_cidr_block)[1]), i)
+    subnet_region         = var.region
+    subnet_private_access = true
+    subnet_flow_logs      = true
+    description           = "Automatically generated subnet"
+  }]
 }
 
 module "vpc-vincent" {
@@ -17,46 +22,16 @@ module "vpc-vincent" {
   network_name = var.vpc_name
   routing_mode = "GLOBAL"
 
-  subnets = [
-    {
-      subnet_name           = local.subnet_01_name
-      subnet_ip             = "10.10.10.0/24"
-      subnet_region         = var.region
-    },
-    {
-      subnet_name           = local.subnet_02_name
-      subnet_ip             = "10.10.20.0/24"
-      subnet_region         = var.region
-      subnet_private_access = true
-      subnet_flow_logs      = true
-      description           = "This subnet has a description"
-    },
-    {
-      subnet_name               = local.subnet_03_name
-      subnet_ip                 = "10.10.30.0/24"
-      subnet_region             = var.region
-      subnet_flow_logs          = true
-      subnet_flow_logs_interval = "INTERVAL_10_MIN"
-      subnet_flow_logs_sampling = 0.7
-      subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
-    }
-  ]
+  subnets = local.subnets
 
   secondary_ranges = {
-    (local.subnet_01_name) = [
-      {
-        range_name    = "subnet-01-secondary-01"
-        ip_cidr_range = "192.168.64.0/24"
-      },
-    ]
-
-    (local.subnet_02_name) = []
+    for subnet in local.subnets : subnet.subnet_name => []
   }
 
   routes = [
     {
       name              = "egress-internet"
-      description       = "route through IGW to access internet"
+      description       = "Route through IGW to access internet"
       destination_range = "0.0.0.0/0"
       tags              = "egress-inet"
       next_hop_internet = true
--- variables.tf
+++ variables.tf
@@ -1,14 +1,34 @@
 variable "project" {
   description = "The GCP project ID"
   type        = string
+  default     = "sandbox-vincentliu"
 }
 
 variable "region" {
   description = "The GCP region"
   type        = string
+  default     = "europe-north1"
 }
 
 variable "vpc_name" {
   description = "The name of the VPC network"
   type        = string
+}
+
+variable "base_cidr_block" {
+  description = "The base CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_prefix_length" {
+  description = "The prefix length for the subnets"
+  type        = number
+  default     = 24
+}
+
+variable "subnet_count" {
+  description = "The number of subnets to create"
+  type        = number
+  default     = 3
 }
