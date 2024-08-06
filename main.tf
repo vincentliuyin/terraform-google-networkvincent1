5c9664c4b9795b5034f24e96a526907dd6c6f85f
liuyinvincent <liu.vincent@devoteam.com>
2024-08-06 16:10:55 +0200

    add main.tf

---
+++ main.tf
@@ -0,0 +1,59 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
+module "vpc-vincent" {
+  source  = "terraform-google-modules/network/google"
+  version = "~> 9.1"
+
+  project_id   = var.project
+  network_name = var.vpc_name
+  routing_mode = "GLOBAL"
+
+  subnets = [
+    {
+      subnet_name           = "subnet-01"
+      subnet_ip             = "10.10.10.0/24"
+      subnet_region         = var.region
+    },
+    {
+      subnet_name           = "subnet-02"
+      subnet_ip             = "10.10.20.0/24"
+      subnet_region         = var.region
+      subnet_private_access = true
+      subnet_flow_logs      = true
+      description           = "This subnet has a description"
+    },
+    {
+      subnet_name               = "subnet-03"
+      subnet_ip                 = "10.10.30.0/24"
+      subnet_region             = var.region
+      subnet_flow_logs          = true
+      subnet_flow_logs_interval = "INTERVAL_10_MIN"
+      subnet_flow_logs_sampling = 0.7
+      subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
+    }
+  ]
+
+  secondary_ranges = {
+    subnet-01 = [
+      {
+        range_name    = "subnet-01-secondary-01"
+        ip_cidr_range = "192.168.64.0/24"
+      },
+    ]
+
+    subnet-02 = []
+  }
+
+  routes = [
+    {
+      name              = "egress-internet"
+      description       = "route through IGW to access internet"
+      destination_range = "0.0.0.0/0"
+      tags              = "egress-inet"
+      next_hop_internet = true
+    }
+  ]
+}
